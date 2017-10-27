a22f4406a764a69b3b4cbbc0a090b2c42f68a789
ErolB <eroljb@hotmail.com>
2017-10-26 22:24:46 -0400

    changes to image retrieval controller

--- modules/grader/server/controllers/image.controller.js
+++ modules/grader/server/controllers/image.controller.js
@@ -6,5 +6,8 @@ var model = model.User;
 exports.next_image = function(req, res){
   var username = req.body['username']; // revisit this
   var record = model.find({'username': username});
-  var session = record['']
-}
+  var session_index = record['currentSessionIndex'];
+  var session = record['session'][session_index];
+  var image_url = record['images'][record['currentImageIndex']];
+  res.body = JSON.stringify({'url': image_url});
+};
