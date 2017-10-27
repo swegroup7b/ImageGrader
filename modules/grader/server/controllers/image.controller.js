7e612c8d8268104861fa9078f5a4d884750e1a89
ErolB <eroljb@hotmail.com>
2017-10-27 12:51:48 -0400

    updates index

--- modules/grader/server/controllers/image.controller.js
+++ modules/grader/server/controllers/image.controller.js
@@ -5,9 +5,18 @@ var model = model.User;
 
 exports.next_image = function(req, res){
   var username = req.body['username']; // revisit this
-  var record = model.find({'username': username});
-  var session_index = record['currentSessionIndex'];
-  var session = record['session'][session_index];
-  var image_url = record['images'][record['currentImageIndex']];
-  res.body = JSON.stringify({'url': image_url});
+  model.find({'username': username}, function(err, record) {
+    if (err) throw err;  // catch errors
+    var session_index = record['currentSessionIndex'];
+    var session = record['session'][session_index];
+    var image_url = record['images'][record['currentImageIndex']];
+    res.body = JSON.stringify({'url': image_url});  // send URL
+    if (record['currentImageIndex'] == record['images'].length-1){
+      record['currentImageIndex'] = 0;
+      record['session'][session_index]['finished'] = true;  // mark current session as finished
+    } else {
+      record['currentImageIndex'] += 1;
+    }
+    record.save();  // update user record
+  });
 };
