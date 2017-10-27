04b63581e503229a4cb8cd5a16262b3023f1969d
ErolB <eroljb@hotmail.com>
2017-10-27 13:19:07 -0400

    updates session duration

--- modules/grader/server/controllers/image.controller.js
+++ modules/grader/server/controllers/image.controller.js
@@ -11,12 +11,14 @@ exports.next_image = function(req, res){
     var session = record['session'][session_index];
     var image_url = record['images'][record['currentImageIndex']];
     res.body = JSON.stringify({'url': image_url});  // send URL
-    if (record['currentImageIndex'] == record['images'].length-1){
+    if (record['currentImageIndex'] == record['images'].length-1){  // if on last image
       record['currentImageIndex'] = 0;
       record['session'][session_index]['finished'] = true;  // mark current session as finished
     } else {
-      record['currentImageIndex'] += 1;
+      record['currentImageIndex'] += 1;  // go to next image
     }
+    current_date = new Date();
+    record['session'][session_index]['duration'] = current_date.getMinutes() - record['session'][session_index]['created'].getMinutes();  // update session duration
     record.save();  // update user record
   });
 };
