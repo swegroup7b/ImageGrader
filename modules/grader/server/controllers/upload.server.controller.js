685ce1023e1aebf5177c078c4a2dad01f9e1458c
Caleb Robey <calebrobey118@gmail.com>
2017-10-30 17:12:47 -0400

    Added the service calls in the grader/upload.server.controller

--- modules/grader/server/controllers/upload.server.controller.js
+++ modules/grader/server/controllers/upload.server.controller.js
@@ -6,11 +6,20 @@
  */
 var path = require('path'),
   mongoose = require('mongoose'),
+  multer = require('multer'),
+  config = require(path.resolve('./config/config')),
   User = mongoose.model('User'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
-
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  session = require('../services/session.js');
 
 exports.upload = function (req, res) {
   console.log(req.files);
+  session.addImage();
+  session.newImage();
+
+  //need more stuff here.
+
+
   res.send();
+
 };
