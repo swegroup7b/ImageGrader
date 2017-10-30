075e57404a0cc86f33aedf54f7acd8201e806d11
Andre Williams <ae.williams@ufl.edu>
2017-10-30 19:44:59 -0400

    working on training page

--- modules/training/client/controllers/training.client.controller.js
+++ modules/training/client/controllers/training.client.controller.js
@@ -5,10 +5,25 @@
     .module('training')
     .controller('TrainingController', TrainingController);
 
-  TrainingController.$inject = ['$scope', '$state', 'UsersService', '$location', '$window', 'Authentication', 'PasswordValidator', 'Notification'];
+  TrainingController.$inject = ['$scope', '$state'];
 
-  function TrainingController($scope, $state, UsersService, $location, $window, Authentication, PasswordValidator, Notification) {
+  function TrainingController($scope, $state) {
     var vm = this;
+ 
+    $scope.currentAnnotation = 0;
+ 
+    $scope.goBack = function(){
+    
+    	$scope.currentAnnotation -= 1;
+    
+    };
+ 
+    $scope.goNext = function(){
 
+    	$scope.currentAnnotation += 1;
+    
+    };
+  
   }
+
 }());
