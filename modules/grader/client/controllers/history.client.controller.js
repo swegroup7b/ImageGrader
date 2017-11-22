c6188036a2f998dd34976abde1730a4af60abbc5
chris8790 <chporwol@gmail.com>
2017-11-21 22:17:23 -0500

    Added page menu and search functionality to result/history pages

--- modules/grader/client/controllers/history.client.controller.js
+++ modules/grader/client/controllers/history.client.controller.js
@@ -5,15 +5,42 @@
     .module('grader')
     .controller('HistoryController', HistoryController);
 
-    function HistoryController($scope, $state, $http, GraderService) {
-      var vm = $scope;
+  HistoryController.$inject = ['$scope', '$state', '$filter', '$http', 'GraderService'];
 
-      GraderService.getSession().then(function(result) {
-        vm.allSessions = result;
-      });
+  function HistoryController($scope, $state, $filter, $http, GraderService) {
+    var vm = $scope;
+    vm.buildPager = buildPager;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+    vm.pageChanged = pageChanged;
 
-      vm.setSessionIndex = function(index) {
-        GraderService.setSessionIndex(index);
-      };
+    GraderService.getSession().then(function(result) {
+      vm.allSessions = result;
+      vm.buildPager();
+    });
+
+    vm.setSessionIndex = function(index) {
+      GraderService.setSessionIndex(index);
+    };
+
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 6;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
     }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.allSessions, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }
+  }
 }());
--- modules/grader/client/controllers/results.client.controller.js
+++ modules/grader/client/controllers/results.client.controller.js
@@ -5,11 +5,39 @@
     .module('grader')
     .controller('ResultsController', ResultsController);
 
-    function ResultsController($scope, $state, $http, GraderService) {
+    ResultsController.$inject = ['$scope', '$state', '$filter', '$http', 'GraderService'];
+
+    function ResultsController($scope, $state, $filter, $http, GraderService) {
       var vm = $scope;
       vm.sessionIndex = GraderService.getSessionIndex();
+      vm.buildPager = buildPager;
+      vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+      vm.pageChanged = pageChanged;
+
       GraderService.getSession().then(function(result) {
-        vm.allSessions = result;
+        vm.images = result[vm.sessionIndex].images;
+        vm.buildPager();
       });
+
+      function buildPager() {
+        vm.pagedItems = [];
+        vm.itemsPerPage = 7;
+        vm.currentPage = 1;
+        vm.figureOutItemsToDisplay();
+      }
+
+      function figureOutItemsToDisplay() {
+        vm.filteredItems = $filter('filter')(vm.images, {
+          $: vm.search
+        });
+        vm.filterLength = vm.filteredItems.length;
+        var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+        var end = begin + vm.itemsPerPage;
+        vm.pagedItems = vm.filteredItems.slice(begin, end);
+      }
+
+      function pageChanged() {
+        vm.figureOutItemsToDisplay();
+      }
     }
 }());
