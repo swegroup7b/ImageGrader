941eb38e4b9f40245918a676020a079ad930382f
Caleb Robey <calebrobey118@gmail.com>
2017-11-27 11:26:33 -0500

    Adding documentation to the README and comments=

--- modules/grader/server/config/upload.server.config.js
+++ modules/grader/server/config/upload.server.config.js
@@ -5,16 +5,13 @@
  */
 var bb = require('express-busboy');
 
-
 /**
  * Module init function
  */
-
 module.exports = function (app) {
-
   bb.extend(app, {
     upload: true,
     path: 'public/uploaded-images',
-    allowedPath: /./,
+    allowedPath: /./
   });
 };
