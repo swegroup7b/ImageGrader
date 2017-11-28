a8710221eb265a874e8336d950267ec4a36c1927
Caleb Bryant <cal2u@ufl.edu>
2017-11-28 01:23:15 -0500

    Fix bug

--- modules/grader/server/controllers/upload.server.controller.js
+++ modules/grader/server/controllers/upload.server.controller.js
@@ -24,7 +24,7 @@ exports.doneUploading = function(req, res) {
   });
 }
 
-//uploads the file. if a session does not exist, a new session is created first.
+// Upload the file. If a session does not exist, a new session is created first.
 exports.upload = function (req, res) {
   console.log(req.files);
   if (session.currentImage(req.user) === undefined) {
@@ -33,37 +33,21 @@ exports.upload = function (req, res) {
     addImage();
   }
 
-//function that adds a new image to the session.
+  // Function that adds a new image to the session.
   function addImage() {
     session.addImage(req.user, {
       name: req.files.file.filename,
       url: req.files.file.file,
       step: 0
     });
+    // Tell client that the image was uploaded successfully
+    res.send();
   }
+};
 
-
-  //may need more stuff here.
-  res.send();
-
-exports.newSession= function(req, res) {
+exports.newSession = function(req, res) {
   console.log("New session controller");
   session.newSession(req.user, function() {
     res.send();
   });
 }
-
-exports.upload = function (req, res) {
-  console.log(req.files);
-  var current = session.currentImage(req.user);
-  console.log("Current image is: "+current);
-
-  session.addImage(req.user, {
-    name: req.files.file.filename,
-    url: req.files.file.file,
-    step: 0
-  }, function() {
-    //may need more stuff here.
-    res.send();
-  });
-};
