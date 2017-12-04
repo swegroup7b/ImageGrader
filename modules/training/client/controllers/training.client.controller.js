d0ed983f7967e7862efec682c8df23deec0e57fb
Andre Williams <ae.williams@ufl.edu>
2017-12-04 13:38:03 -0500

    working on training page

--- modules/training/client/controllers/training.client.controller.js
+++ modules/training/client/controllers/training.client.controller.js
@@ -5,31 +5,40 @@
     .module('training')
     .controller('TrainingController', TrainingController);
 
-  //TrainingController.$inject = ['$scope', '$state', 'Gra'];
 
   function TrainingController($scope, $state, GraderService) {
     var vm = this;
+
     $scope.currentAnnotation = 0;
+
     $scope.goBack = function() {
       $scope.currentAnnotation -= 1;
-      console.log("On step: "+$scope.on.step);
       $scope.on.step--;
       $scope.annotations[$scope.on.step] = new GraderService.Annotation($scope.on.step);
-      console.log("Now on step: "+$scope.on.step);
       $scope.redraw();
     };
+
     $scope.goNext = function() {
       $scope.currentAnnotation += 1;
       $scope.on.step++;
     };
+
     $scope.goFirst = function() {
       $scope.currentAnnotation = 0;
       $scope.on.step = 0;
+      $scope.annotations = []
+      var annotationSteps = GraderService.annotationSteps();
+      for (var i = 0; i < annotationSteps.length; i++) {
+        $scope.annotations.push(new GraderService.Annotation(i));
+      }
+      $scope.redraw();
     };
+
     $scope.on = {
       url: "public/modules/training/client/img/grader/D4_KDA_1.jpg",
       step: 0
     };
+
     $scope.annotations = []
     var annotationSteps = GraderService.annotationSteps();
     for (var i = 0; i < annotationSteps.length; i++) {
