13fe3d55bec043f2f4cfa62683074322ddfd19cd
Andre Williams <ae.williams@ufl.edu>
2017-12-07 12:59:08 -0500

    working on training page

--- modules/training/client/controllers/training.client.controller.js
+++ modules/training/client/controllers/training.client.controller.js
@@ -11,6 +11,7 @@
 
     $scope.currentAnnotation = 0;
 
+    //Go back to the previous grading step
     $scope.goBack = function() {
       $scope.currentAnnotation -= 1;
       $scope.on.step--;
@@ -18,11 +19,13 @@
       $scope.redraw();
     };
 
+    //Move on to the next grading step
     $scope.goNext = function() {
       $scope.currentAnnotation += 1;
       $scope.on.step++;
     };
 
+    //Restarts the grading from the first step
     $scope.goFirst = function() {
       $scope.currentAnnotation = 0;
       $scope.on.step = 0;
@@ -34,17 +37,20 @@
       $scope.redraw();
     };
 
+    //Sets the image used by the grader on the training page
     $scope.on = {
       url: "public/modules/training/client/img/grader/D4_KDA_1.jpg",
       step: 0
     };
 
+    //Adds the grading steps to the training page from the grader module
     $scope.annotations = []
     var annotationSteps = GraderService.annotationSteps();
     for (var i = 0; i < annotationSteps.length; i++) {
       $scope.annotations.push(new GraderService.Annotation(i));
     }
 
+    //Moves on to the next grading step and saves the annotation
     $scope.addAnnotation = function() {
       $scope.goNext();
       $scope.$apply();
